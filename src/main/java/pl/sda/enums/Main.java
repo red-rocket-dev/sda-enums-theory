992bd133c7d63effc68dc40c7c69b54a8aea1fb3
Jakub <kubplon@gmail.com>
2020-04-10 16:24:30 +0200

    constructor

--- src/main/java/pl/sda/enums/Main.java
+++ src/main/java/pl/sda/enums/Main.java
@@ -2,7 +2,7 @@ package pl.sda.enums;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: pola w enumach
+        //TODO: pola w enumach + konstruktor
         //TODO: wszystkie metody enumow
         //TODO: enum w klasie
         //TODO: livecoding - miniaplikacja do zarzadzania flota
