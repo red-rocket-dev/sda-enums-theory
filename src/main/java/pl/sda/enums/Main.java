9d9d8633fff3052bc4141790772fdc79b202f244
Jakub <kubplon@gmail.com>
2020-04-10 16:53:52 +0200

    toString?

--- src/main/java/pl/sda/enums/Main.java
+++ src/main/java/pl/sda/enums/Main.java
@@ -3,6 +3,7 @@ package pl.sda.enums;
 public class Main {
     public static void main(String[] args) {
         //TODO: pola w enumach + konstruktor
+        //TODO: jak z toString()?
         //TODO: wszystkie metody enumow
         //TODO: enum w klasie
         //TODO: livecoding - miniaplikacja do zarzadzania flota
